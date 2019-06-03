f322dd640bbaa245938f3ef59d36a3d71cde3f83
Santiago G. Marín <santiago.marin@dotstudio.io>
2019-06-03 15:20:11 -0400

    Updated compiled templates

--- test/templates/dest/simple.js
+++ test/templates/dest/simple.js
@@ -1,14 +1,14 @@
 const pug = require('pug-runtime');
 
-module.exports = function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;pug_debug_line = 1;pug_debug_filename = "\u002Fhome\u002Fsantiago\u002FWorkspace\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
+module.exports = function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;pug_debug_line = 1;pug_debug_filename = "\u002Fhome\u002Fstgogm\u002FWorkspace\u002Ffinal\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
 pug_html = pug_html + "\u003C!DOCTYPE html\u003E";
-;pug_debug_line = 3;pug_debug_filename = "\u002Fhome\u002Fsantiago\u002FWorkspace\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
-pug_html = pug_html + "\u003Chtml\u003E";
-;pug_debug_line = 4;pug_debug_filename = "\u002Fhome\u002Fsantiago\u002FWorkspace\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
+;pug_debug_line = 3;pug_debug_filename = "\u002Fhome\u002Fstgogm\u002FWorkspace\u002Ffinal\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
+pug_html = pug_html + "\u003Chtml lang=\"en\"\u003E";
+;pug_debug_line = 4;pug_debug_filename = "\u002Fhome\u002Fstgogm\u002FWorkspace\u002Ffinal\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
 pug_html = pug_html + "\u003Chead\u003E";
-;pug_debug_line = 5;pug_debug_filename = "\u002Fhome\u002Fsantiago\u002FWorkspace\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
+;pug_debug_line = 5;pug_debug_filename = "\u002Fhome\u002Fstgogm\u002FWorkspace\u002Ffinal\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
 pug_html = pug_html + "\u003Ctitle\u003E";
-;pug_debug_line = 5;pug_debug_filename = "\u002Fhome\u002Fsantiago\u002FWorkspace\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
+;pug_debug_line = 5;pug_debug_filename = "\u002Fhome\u002Fstgogm\u002FWorkspace\u002Ffinal\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
 pug_html = pug_html + "Gulp Pug Module Test Layout\u003C\u002Ftitle\u003E\u003C\u002Fhead\u003E";
-;pug_debug_line = 7;pug_debug_filename = "\u002Fhome\u002Fsantiago\u002FWorkspace\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
+;pug_debug_line = 7;pug_debug_filename = "\u002Fhome\u002Fstgogm\u002FWorkspace\u002Ffinal\u002Fgulp-pug-module\u002Ftest\u002Ftemplates\u002Fsource\u002Fsimple.pug";
 pug_html = pug_html + "\u003Cbody\u003E\u003C\u002Fbody\u003E\u003C\u002Fhtml\u003E";} catch (err) {pug.rethrow(err, pug_debug_filename, pug_debug_line);};return pug_html;};
